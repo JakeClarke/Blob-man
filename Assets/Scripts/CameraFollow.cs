b64e6d187fdab5e678af5ee588b58c2fd00e6c97
Dan Ursu <10077734@brookes.ac.uk>
2013-03-06 19:07:08 +0000

    Camera now follows the player left and right..

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,16 +6,43 @@ public class CameraFollow : MonoBehaviour {
 	public Transform target;
 	public float distance = 10;
 	public float moveAmount = 2;
+	public float fastForward = 12;
+	public float fastForwardDelay = 1;
+	
+	
+	private bool moveRight = true;
 	
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.RightArrow)){
+			moveRight = true;
+		} else if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			moveRight = false;	
+		}
+	}
+	
 	// Update is called once per frame
 	void LateUpdate () {
-		if (transform.position.x - target.position.x < distance){
-			transform.Translate(moveAmount * Time.deltaTime, 0, 0);	
-		}		
+		if (moveRight){
+			if (transform.position.x - target.position.x < distance){
+				if (transform.position.x - target.position.x < distance - fastForwardDelay){
+					transform.Translate(fastForward * Time.deltaTime, 0, 0);	
+				} else {
+					transform.Translate(moveAmount * Time.deltaTime, 0, 0);	
+				}
+			}		
+		} else {
+			if (target.position.x - transform.position.x < distance){
+				if (target.position.x - transform.position.x < distance - fastForwardDelay){
+					transform.Translate(-fastForward * Time.deltaTime, 0, 0);	
+				} else {
+					transform.Translate(-moveAmount * Time.deltaTime, 0, 0);	
+				}
+			}	
+		}
 	}
 }
