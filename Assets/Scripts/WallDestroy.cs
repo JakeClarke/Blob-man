e5b75003ca21429113fdda9eda82e77f519ba69c
Jake Clarke <clarke.jake@gmail.com>
2013-04-30 22:13:57 +0100

    Improved wall smashing explosion logic.
    Now uses the contact normal to calculate the explosion direction as well
    as the force the wall is hit with.

--- Assets/Scripts/WallDestroy.cs
+++ Assets/Scripts/WallDestroy.cs
@@ -4,32 +4,24 @@ using System.Collections;
 public class WallDestroy : MonoBehaviour {
 	
 	public int numberOfBricks = 10;
-	public float explosionForce = 100f;
+	public float explosionForceScaler = 1f;
 	public float explosionSize = 10f;
 	public GameObject brickPrefab;
 	public GameObject smokePrefab;
 	public float breakThreshold = 20f;
-	public Vector3 explosionOffset = new Vector3(-10, 10, 0);
-	private Vector3 _posOffset = new Vector3(0f, 10f,0f);
+	public float explosionDirScaler = -10f;
+	private Vector3 _posOffset = new Vector3(0f, 1f,0f);
 	
 	void OnCollisionEnter(Collision collision) {
 		if (collision.collider.tag == "Player" && collision.impactForceSum.x > breakThreshold) {
-			Break();
+			Debug.Log (collision.impactForceSum.magnitude);
+			for (int i = 0; i < this.numberOfBricks; i++) {
+				GameObject b = (GameObject)Instantiate(this.brickPrefab, this.transform.position + this._posOffset + Random.insideUnitSphere, Random.rotation);
+				b.rigidbody.AddExplosionForce(collision.relativeVelocity.magnitude * explosionForceScaler, this.transform.position + (explosionDirScaler * collision.contacts[0].normal), explosionSize);
+			}
+			Instantiate(smokePrefab, this.transform.position, Quaternion.identity);
+			
+			Destroy(this.gameObject);
 		}	
-	}
-	
-	public void Break() {
-		
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		
-		for (int i = 0; i < this.numberOfBricks; i++) {
-			GameObject b = (GameObject)Instantiate(this.brickPrefab, this.transform.position + this._posOffset + Random.insideUnitSphere, Random.rotation);
-			Physics.IgnoreCollision(b.collider, player.collider);
-			b.rigidbody.AddExplosionForce(this.explosionForce, this.transform.position + explosionOffset, explosionSize);
-		}
-		
-		Instantiate(smokePrefab, this.transform.position, Quaternion.identity);
-		
-		Destroy(this.gameObject);
 	}
 }
