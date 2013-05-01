8607ce9212cd189909d2fd7916ea5c3e966d9953
Dan Ursu <10077734@brookes.ac.uk>
2013-05-01 13:57:09 +0100

    added rotation based on the direction

--- Assets/Scripts/Enemy/AI.cs
+++ Assets/Scripts/Enemy/AI.cs
@@ -7,23 +7,27 @@ public class AI : MonoBehaviour {
 	public float speed = 4;
 	public float distanceToMove = 10;
 	
+	private const float _RIGHTANGLE = 180;
+	private const float _LEFTANGLE = 0;
+	
 	private float startPoint;
 
 	// Use this for initialization
 	void Start () {
 		startPoint = transform.position.x;
+		transform.eulerAngles = new Vector3(0, _LEFTANGLE, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if (transform.position.x - startPoint >= distanceToMove){
-			speed = -speed;
+			transform.eulerAngles = new Vector3(0, _LEFTANGLE, 0);
 		} else if ( startPoint - transform.position.x >= distanceToMove) {
-			speed = -speed;
+			transform.eulerAngles = new Vector3(0, _RIGHTANGLE, 0);
 		}
 		
-		transform.Translate(speed * Time.deltaTime, 0, 0);
+		transform.Translate(-speed * Time.deltaTime, 0, 0);
 	}
 	
 	void FixedUpdate(){	
