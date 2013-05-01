7367e1482c94c39a69a34c5ba1bffc7720b30abe
Jake Clarke <clarke.jake@gmail.com>
2013-05-01 21:47:59 +0100

    Included the reason for the gameover on screen.
    Improved the positioning of the game over message on screen.
    Improved the logic for setting up the game over screen to make it
    testable.

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -4,21 +4,30 @@ using System.Collections;
 public class GameOverMenu : MonoBehaviour {
 	
 	public string levelToLoad = "LevelScene";
+	public Vector2 boxSize = new Vector2(200f,100f);
+	public Vector2 buttonSize = new Vector2(70f, 35f);
 	
+	private string reason;
 	private float score;
 	
 	void Start () {
 		GameObject scoreEnt = GameObject.FindGameObjectWithTag("Score");
-		ScoreController scoreController = scoreEnt.GetComponent("ScoreController") as ScoreController;
-		this.levelToLoad = scoreController.CurrentLevel;
-		this.score = scoreController.Score;
-		Destroy(scoreEnt);		
+		if (scoreEnt != null) {
+			ScoreController scoreController = scoreEnt.GetComponent("ScoreController") as ScoreController;
+			this.levelToLoad = scoreController.CurrentLevel;
+			this.score = scoreController.Score;
+			this.reason = scoreController.GameOverReason;
+			Destroy(scoreEnt);
+		}		
 	}
 	
 	void OnGUI () {
-		GUI.Box(new Rect(0,0, 140,70), "Game Over! Score: " + this.score);
-		if(GUI.Button(new Rect(10,40,100,30), "Restart")) {
+		GUI.BeginGroup(new Rect (Screen.width / 2 - (boxSize.x / 2), Screen.height / 2 - (boxSize.y / 2), boxSize.x, boxSize.y));
+		GUI.Box(new Rect(0,0, boxSize.x, boxSize.y), "Game Over!\n"+ reason +"\nScore: " + this.score);
+		if(GUI.Button(new Rect(boxSize.x / 2 - buttonSize.x /2,  60, buttonSize.x, buttonSize.y), "Restart")) {
 			Application.LoadLevel(levelToLoad);
 		}
+		
+		GUI.EndGroup();
 	}
 }
